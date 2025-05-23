e813327b42d9e1d0ba6def1e112da03fdbed30aa
Nanh <nanh281103@gmail.com>
2025-05-23 11:11:15 +0700

    export excel for total revenue each month

--- src/main/java/com/nanhng/FastFood/controller/DashboardController.java
+++ src/main/java/com/nanhng/FastFood/controller/DashboardController.java
@@ -1,12 +1,16 @@
 package com.nanhng.FastFood.controller;
 
+import com.nanhng.FastFood.dto.constant.RoleType;
 import com.nanhng.FastFood.dto.response.BaseResponse;
 import com.nanhng.FastFood.dto.response.dashboard.OrderCountRes;
 import com.nanhng.FastFood.dto.response.dashboard.OrderRevenueRes;
 import com.nanhng.FastFood.dto.response.dashboard.ProductRevenueRes;
+import com.nanhng.FastFood.entity.user.User;
 import com.nanhng.FastFood.service.dashboard.DashboardService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +41,11 @@ public class DashboardController {
     @GetMapping("v1/dashboard/product-count")
     public ResponseEntity<BaseResponse<List<OrderCountRes>>> countNumberOfOrder(){
         return ResponseEntity.ok(new BaseResponse<>(dashboardService.countOrderByMonth(),"count number of orders each month"));
+    }
+    @Operation(summary = "export data of revenue each month as excel")
+    @GetMapping("v1/dashboard/export-total-revenue-by-month")
+    public ResponseEntity<InputStreamResource> exportTotalRevenueByMonth() {
+        return ResponseEntity.ok().contentType(MediaType.valueOf("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .body(new InputStreamResource(dashboardService.excelTotalRevenueByMonth()));
     }
 }
--- src/main/java/com/nanhng/FastFood/service/dashboard/DashboardServiceImpl.java
+++ src/main/java/com/nanhng/FastFood/service/dashboard/DashboardServiceImpl.java
@@ -6,15 +6,20 @@ import com.nanhng.FastFood.dto.response.dashboard.OrderRevenueRes;
 import com.nanhng.FastFood.dto.response.dashboard.ProductRevenueRes;
 import com.nanhng.FastFood.entity.user.User;
 import com.nanhng.FastFood.exception.LovelyException;
+import com.nanhng.FastFood.other_service.excel.ExcelService;
 import com.nanhng.FastFood.repository.order.OrderRepository;
 import com.nanhng.FastFood.repository.orderItem.OrderItemRepository;
 import com.nanhng.FastFood.service.BaseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DashboardServiceImpl extends BaseService implements DashboardService {
@@ -42,4 +47,61 @@ public class DashboardServiceImpl extends BaseService implements DashboardServic
 
         return orderRepository.countNumberOrderByMonth();
     }
+
+    @Override
+    public InputStream excelTotalRevenueByMonth() {
+        User user = getUser(RoleType.ADMIN);
+
+        List<OrderRevenueRes> list = orderRepository.calculateRevenueByMonth();
+        try {
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet =workbook.createSheet("Revenue");
+            CellStyle style = MyCellStyle(workbook);
+            sheet.setDefaultColumnWidth(30);
+            Row row = sheet.createRow(0);
+            ExcelService.createCell(row, "STT",0, style);
+
+            ExcelService.createCell(row, "Tháng",1, style);
+
+            ExcelService.createCell(row, "Doanh thu",2, style);
+
+            for(int i = 0; i < list.size(); i++) {
+                row = sheet.createRow(i+1);
+
+                ExcelService.createCell(row, String.valueOf(i+1),0, style);
+                ExcelService.createCell(row, list.get(i).getDate().toString(),1, style);
+                ExcelService.createCell(row, list.get(i).getRevenue().toString(),1, style);
+            }
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            workbook.write(out);
+            workbook.close();
+            return new ByteArrayInputStream(out.toByteArray());
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new LovelyException("can not create excel file");
+        }
+    }
+
+    private CellStyle MyCellStyle(Workbook workbook) {
+        CellStyle style = workbook.createCellStyle();
+        // Set black borders
+        style.setBorderTop(BorderStyle.THIN);
+        style.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderRight(BorderStyle.THIN);
+        style.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        style.setAlignment(HorizontalAlignment.LEFT);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        Font font = workbook.createFont();
+        font.setFontName("Times New Roman");
+        font.setFontHeightInPoints((short) 12);
+        style.setFont(font);
+        style.setDataFormat(workbook.createDataFormat().getFormat("@"));
+        return style;
+    }
+
 }
